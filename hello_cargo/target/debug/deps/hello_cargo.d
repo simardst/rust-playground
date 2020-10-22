0f945e2d70ff4ac41fa62097bc4a0d829f0df73c
Stephane Simard <ssimard@genetec.com>
2020-10-22 10:03:36 -0400

    Hello Cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\DEV\GITHUB\rust-playground\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\DEV\GITHUB\rust-playground\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\DEV\GITHUB\rust-playground\hello_cargo\target\debug\hello_cargo.exe: C:\DEV\GITHUB\rust-playground\hello_cargo\src\main.rs
